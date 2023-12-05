15caa17aef487298a3a41f44e56a50ac6f423bd7
dddochi <86943499+dddochi@users.noreply.github.com>
2023-12-05 17:50:28 +0900

    make ItemModel

---
+++ lib/model/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  final String itemId;
+  final String fakeName;
+  final String type;
+  final String imagePath; // 5 - means : the number of total image
+
+  ItemModel({
+    required this.itemId,
+    required this.fakeName,
+    required this.type,
+    required this.imagePath,
+  });
+}

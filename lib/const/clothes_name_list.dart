f263cffb3bf9e7c22481384610630c5382134ae4
dddochi <86943499+dddochi@users.noreply.github.com>
2023-12-08 11:05:52 +0900

    change clothes_name_list

--- lib/const/clothes_name_list.dart
+++ lib/const/clothes_name_list.dart
@@ -1,9 +1,11 @@
+import 'package:ml_project/const/fake_name/%20culottes_list.dart';
 import 'package:ml_project/const/fake_name/ballgown_list.dart';
 import 'package:ml_project/const/fake_name/blazer_list.dart';
 import 'package:ml_project/const/fake_name/blouse_list.dart';
 import 'package:ml_project/const/fake_name/blouson_list.dart';
 import 'package:ml_project/const/fake_name/bomber_list.dart';
 import 'package:ml_project/const/fake_name/buttondown_list.dart';
+import 'package:ml_project/const/fake_name/caftan_list.dart';
 import 'package:ml_project/const/fake_name/cami_list.dart';
 import 'package:ml_project/const/fake_name/cape_list.dart';
 import 'package:ml_project/const/fake_name/cardigan_list.dart';
@@ -11,8 +13,10 @@ import 'package:ml_project/const/fake_name/coat_list.dart';
 import 'package:ml_project/const/fake_name/crewneck_list.dart';
 import 'package:ml_project/const/fake_name/culotte_list.dart';
 import 'package:ml_project/const/fake_name/down_list.dart';
+import 'package:ml_project/const/fake_name/dress_list.dart';
 import 'package:ml_project/const/fake_name/duster_list.dart';
 import 'package:ml_project/const/fake_name/frock_list.dart';
+import 'package:ml_project/const/fake_name/gown_list.dart';
 import 'package:ml_project/const/fake_name/henley_list.dart';
 import 'package:ml_project/const/fake_name/hoodie_list.dart';
 import 'package:ml_project/const/fake_name/jacket_list.dart';
@@ -22,11 +26,13 @@ import 'package:ml_project/const/fake_name/jumpsuit_list.dart';
 import 'package:ml_project/const/fake_name/kaftan_list.dart';
 import 'package:ml_project/const/fake_name/kimono_list.dart';
 import 'package:ml_project/const/fake_name/knit_list.dart';
+import 'package:ml_project/const/fake_name/legging_list.dart';
 import 'package:ml_project/const/fake_name/leggings_list.dart';
 import 'package:ml_project/const/fake_name/maxi_list.dart';
 import 'package:ml_project/const/fake_name/midi_list.dart';
 import 'package:ml_project/const/fake_name/overalls_list.dart';
 import 'package:ml_project/const/fake_name/overcoat_list.dart';
+import 'package:ml_project/const/fake_name/pant_list.dart';
 import 'package:ml_project/const/fake_name/pants_list.dart';
 import 'package:ml_project/const/fake_name/parka_list.dart';
 import 'package:ml_project/const/fake_name/peacoat_list.dart';
@@ -34,20 +40,25 @@ import 'package:ml_project/const/fake_name/poncho_list.dart';
 import 'package:ml_project/const/fake_name/print_list.dart';
 import 'package:ml_project/const/fake_name/pullover_list.dart';
 import 'package:ml_project/const/fake_name/romper_list.dart';
+import 'package:ml_project/const/fake_name/sheath_list.dart';
 import 'package:ml_project/const/fake_name/shift_list.dart';
 import 'package:ml_project/const/fake_name/shirt_list.dart';
 import 'package:ml_project/const/fake_name/shirtdress_list.dart';
+import 'package:ml_project/const/fake_name/skirt_list.dart';
 import 'package:ml_project/const/fake_name/skirts_list.dart';
 import 'package:ml_project/const/fake_name/skort_list.dart';
 import 'package:ml_project/const/fake_name/suit_list.dart';
 import 'package:ml_project/const/fake_name/sweater_list.dart';
+import 'package:ml_project/const/fake_name/sweatershirt_list.dart';
 import 'package:ml_project/const/fake_name/sweatpants_list.dart';
 import 'package:ml_project/const/fake_name/sweatshirt_list.dart';
+import 'package:ml_project/const/fake_name/t_shirt_list.dart';
 import 'package:ml_project/const/fake_name/tank_list.dart';
 import 'package:ml_project/const/fake_name/tee_list.dart';
 import 'package:ml_project/const/fake_name/top_list.dart';
 import 'package:ml_project/const/fake_name/trench_list.dart';
 import 'package:ml_project/const/fake_name/trouser_list.dart';
+import 'package:ml_project/const/fake_name/trousers_list.dart';
 import 'package:ml_project/const/fake_name/tunic_list.dart';
 import 'package:ml_project/const/fake_name/turtleneck_list.dart';
 import 'package:ml_project/const/fake_name/vest_list.dart';
@@ -60,19 +71,19 @@ final clothesNameList = [
   blouson_list,
   bomber_list,
   buttondown_list,
-  //caftan_list,
+  caftan_list, //
   cami_list,
   cape_list,
   cardigan_list,
   coat_list,
   crewneck_list,
   culotte_list,
-  //culottes_list,
+  culottes_list, //
   down_list,
-  //dress_list,
+  dress_list,
   duster_list,
   frock_list,
-  //gown_list,
+  gown_list,
   henley_list,
   hoodie_list,
   jacket_list,
@@ -83,12 +94,12 @@ final clothesNameList = [
   kimono_list,
   knit_list,
   leggings_list,
-  //legging_list,
+  legging_list, //
   maxi_list,
   midi_list,
   overalls_list,
   overcoat_list,
-  //pant_list,
+  pant_list, //
   pants_list,
   parka_list,
   peacoat_list,
@@ -96,25 +107,25 @@ final clothesNameList = [
   print_list,
   pullover_list,
   romper_list,
-  //sheath_list,
+  sheath_list,
   shift_list,
   shirt_list,
   shirtdress_list,
-  //skirt_list,
+  skirt_list, //
   skirts_list,
   skort_list,
   suit_list,
   sweater_list,
-  //sweatershirt_list,
+  sweatershirt_list,
   sweatpants_list,
   sweatshirt_list,
-  //t_shirt_list,
+  t_shirt_list,
   tank_list,
   tee_list,
   top_list,
   trench_list,
   trouser_list,
-  //trousers_list,
+  trousers_list,
   tunic_list,
   turtleneck_list,
   vest_list,

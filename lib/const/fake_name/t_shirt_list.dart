ed234c654303087489363b34ed45c9e979c81dc7
dddochi <86943499+dddochi@users.noreply.github.com>
2023-12-08 11:03:53 +0900

    update all data and t_shirt  (asset / pubspec.yaml)

---
+++ lib/const/fake_name/t_shirt_list.dart
@@ -0,0 +1,7 @@
+final t_shirt_list = [
+  {"item-id": "2814609_1", "type": "t_shirt", "fake-name": "glamorous minimalist urban bohemian t-shirt", "image-path": "asset/clothes/t_shirt/4.jpeg"},
+  {"item-id": "2814609_20", "type": "t_shirt", "fake-name": "fashionable relaxed lively poised t-shirt", "image-path": "asset/clothes/t_shirt/1.jpeg"},
+  {"item-id": "2814609_8", "type": "t_shirt", "fake-name": "sleek urban sleek sleek t-shirt", "image-path": "asset/clothes/t_shirt/3.jpeg"},
+  {"item-id": "2814609_16", "type": "t_shirt", "fake-name": "striking dynamic sharp whimsical t-shirt", "image-path": "asset/clothes/t_shirt/1.jpeg"},
+  {"item-id": "2814609_8", "type": "t_shirt", "fake-name": "sleek urban sleek sleek t-shirt", "image-path": "asset/clothes/t_shirt/3.jpeg"}
+];
--- lib/const/random_list.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ml_project/const/clothes_list.dart';
-
-//clothes & item_id & Strnig_name
-final dic = {
-  CLOTHES.top.name: ['1234', '2456', '1567'],
-  CLOTHES.blazer.name: ['3456', '4587', '9090'],
-  CLOTHES.bomber.name: ['12-333', '8021'],
-  CLOTHES.cami.name: ['33333', '3332323'],
-};
